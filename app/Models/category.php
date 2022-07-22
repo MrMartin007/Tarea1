08c36bc119e22bb447d91f34067b41fe17cebcc5
MrMartiin <mrmartiin007@gmail.com>
2022-07-22 13:34:52 -0600

    modelos

--- app/Models/category.php
+++ app/Models/category.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class category extends Model
 {
-    use HasFactory;
+    protected $table='category';
+    public $timestamps=false;
+    protected $fillable=[
+        'id','description',
+    ];
+    protected $primaryKey = 'id';
 }
--- app/Models/customer.php
+++ app/Models/customer.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class customer extends Model
 {
-    use HasFactory;
+    protected $table='customer';
+    public $timestamps=false;
+    protected $fillable=[
+        'id','name','addres','phone_number','category_id',
+    ];
+    protected $primaryKey = 'id';
 }
