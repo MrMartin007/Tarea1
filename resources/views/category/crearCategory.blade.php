83e871f766febc799249dd159062618025140d5e
MrMartiin <mrmartiin007@gmail.com>
2022-07-22 19:44:32 -0600

    vista category actualizacion

--- resources/views/category/crearCategory.blade.php
+++ resources/views/category/crearCategory.blade.php
@@ -7,56 +7,69 @@
 @stop
 
 @section('content')
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-7 mt-5">
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
 
-                <!--Validacion de errores-->
-                @if($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li>{{$error}}</li>
-                            @endforeach
-                        </ul>
+        </div>
+    @endif
+
+    <div style="height: 20px;"></div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="card shadow-lg p-3 mb-5 bg-white ">
+                    <div class="card-header">AGREGAR CATEGORIA</div>
+                    <div class="card-body">
+                        <form action="{{ route('Category.save')}}" method="POST" enctype="multipart/form-data" class="alerta">
+                            @csrf
+                            <div class="form-row">
+                                <div class="col-md-5 mb-4 ">
+                                    <label for="nombre" >Descripcion </label>
+                                    <input type="text" name="description" class="form-control" placeholder="holaa"  required>
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary offset-3 "  >
+                                <i class="fas fa-plus">  Guardar</i>
+                            </button>
+                            <a class="btn btn-primary  offset-2" href="{{url('/listaCategory')}}" role="button">
+                                <i  class="fas fa-arrow-left"> Regresar</i>
+                            </a>
+                        </form>
 
                     </div>
-                @endif
-
-
-                <div class="card border-success">
-                    <form action="{{ route('Category.save')}}" method="POST" enctype="multipart/form-data" class="alerta">
-                        @csrf
-
-                        <div class="card-header border-success text-center text-white" style="background-color: #196F3D"; >AGREGAR CATEGORY</div>
-
-                        <div class="card-body" style="background-color: #D4EFDF;">
-
-                            <div class="row form-group">
-                                <label for="" class="col-2">Description</label>
-                                <input type="text" name="description" class="form-control col-md-9">
-                            </div>
-
-
-                            <div class="row form-group">
-                                <button type="submit" class="btn btn col-md-9 offset-2 text-dark mb-2" style="background-color: #58D68D;" >
-                                    <i class="fas fa-plus">  Guardar</i>
-                                </button>
-
-                                <a class="btn btn-outline-secondary col-md-9 offset-2 text-dark" href="{{url('/curso')}}" role="button">
-                                    <i class="far fa-hand-point-left"> Regresar</i>
-                                </a>
-                            </div>
-                        </div>
-                    </form>
                 </div>
-
             </div>
         </div>
-    </div>
 @stop
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            height: 100%;
+            margin: 0;
+            font-family: Lato, sans-serif;
+            background-color:     #E1E2E1;
+        }
+        header{
+            background: #1488CC;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #2B32B2, #1488CC);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #2B32B2, #1488CC); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        }
+
+        .card-header{
+            background: #00bc8c;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right,#1c7430, #1488CC);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #3a4047, #4e555b); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            color:white;
+        }
+    </style>
 @stop
 @section('js')
     <script> console.log('Hi!'); </script>
--- resources/views/category/editCategory.blade.php
+++ resources/views/category/editCategory.blade.php
@@ -6,56 +6,70 @@
     <h1>Editar Category</h1>
 @stop
 @section('content')
-    <div class="container mt-5">
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+
+        </div>
+    @endif
+    <div style="height: 20px;"></div>
+    <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-7 mt-5">
-
-
-                <!--Validacion de errores-->
-                @if($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li>{{$error}}</li>
-                            @endforeach
-                        </ul>
-
-                    </div>
-                @endif
-
-                <div class="card border-primary">
+            <div class="col-lg-8">
+                <div class="card shadow-lg p-3 mb-5 bg-white ">
+                    <div class="card-header">EDITAR CATEGORIA</div>
+                    <div class="card-body">
                     <form action="{{ route('editCategory', $category->id)}}" method="POST" enctype="multipart/form-data">
                         @csrf @method('PATCH')
-
-                        <div class="card-header border-primary text-center text-white" style="background-color: #21618C;" >MODIFICAR CATEGORIA</div>
-
-                        <div class="card-body" style="background-color: #D6EAF8;">
-
-                            <div class="row form-group">
-                                <label for="" class="col-2">Descripcion</label>
-                                <input type="text" name="description" class="form-control col-md-9" value="{{$category->description}}">
+                        <div class="form-row">
+                            <div class="col-md-5 mb-4 ">
+                                <label for="nombre" >Descripcion </label>
+                                <input type="text" name="description" class="form-control" value="{{$category->description}}">
                             </div>
-
-                            <div class="row form-group">
-                                <button type="submit" class="btn btn-primary text-dark col-md-9 offset-2 mb-2" style="background-color: #5499C7;">
-                                    <i class="fas fa-undo"> Modificar</i>
-                                </button>
-
-                                <a class="btn btn-outline-secondary col-md-9 offset-2 text-dark" href="{{url('/category')}}" role="button">
-                                    <i class="far fa-hand-point-left"> Regresar</i>
-                                </a>
-                            </div>
-
                         </div>
+
+                            <button type="submit" class="btn btn-primary offset-3 "  >
+                                <i class="fas fa-plus">  Guardar</i>
+                            </button>
+                            <a class="btn btn-primary  offset-2" href="{{url('/listaCategory')}}" role="button">
+                                <i  class="fas fa-arrow-left"> Regresar</i>
+                            </a>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+
 @stop
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            height: 100%;
+            margin: 0;
+            font-family: Lato, sans-serif;
+            background-color:     #E1E2E1;
+        }
+        header{
+            background: #1488CC;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #2B32B2, #1488CC);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #2B32B2, #1488CC); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        }
+
+        .card-header{
+            background: #00bc8c;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right,#1c7430, #1488CC);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #3a4047, #4e555b); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            color:white;
+        }
+    </style>
+
 @stop
 
 @section('js')
